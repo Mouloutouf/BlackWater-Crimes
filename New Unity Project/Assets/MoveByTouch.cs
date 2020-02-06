bdf848519b8ceffd390cf50e1bc31bfc7bf1b4a3
Maxence <maxdurlon@gmail.com>
2020-02-06 16:24:31 +0100

    Mobile connecté // bugs unity incompréhensibles
    (+ un PC de merde à 15 FPS pour bosser)

---
+++ New Unity Project/Assets/MoveByTouch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour
+{
+    public Camera theCam;
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPos.z = 0f;
+            transform.position = touchPos;
+        }
+    }
+}
