6d72a75c3f1e92285610f659daa6c0083f7c2b8e
Maxence <maxdurlon@gmail.com>
2020-02-08 20:53:42 +0100

    Zoom fonctionnel / Drag (W.I.P)

--- Dev/New Unity Project/Assets/Scripts/MoveByTouch.cs
+++ Dev/New Unity Project/Assets/Scripts/MoveByTouch.cs
@@ -8,6 +8,15 @@ public class MoveByTouch : MonoBehaviour
 
     void Update()
     {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.touches[i];
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+            Debug.DrawLine(Vector3.zero, touchPos, Color.red);
+        }
+
+
+        /*
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Touch touch = Input.GetTouch(0);
@@ -15,5 +24,6 @@ public class MoveByTouch : MonoBehaviour
             Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
             transform.position = touchPos;
         }
+        */
     }
 }
