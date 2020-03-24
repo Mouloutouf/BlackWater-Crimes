122e1220b9c6bcfbb535a2ccd17596ed49cc6205
Eliot <ringevaleliot@hotmail.fr>
2020-03-24 11:35:29 +0100

    NeighborhoodScene + Linking Scenes

---
+++ Dev/BlackWater Crimes Project/Assets/Scripts/EliotDev/NeighborhoodSampleScene/NeighborhoodValidateButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NeighborhoodValidateButton : MonoBehaviour
+{
+    [SerializeField] Dropdown dropdown;
+    [SerializeField] Text dialogueText;
+
+    public void Validate()
+    {
+        dialogueText.text = "I will go there! Anything else?";
+        dropdown.value = 0;
+    }
+}
