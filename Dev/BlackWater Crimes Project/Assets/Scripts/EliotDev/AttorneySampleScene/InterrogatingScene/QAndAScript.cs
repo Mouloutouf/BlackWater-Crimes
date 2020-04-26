65a0187df5b295a8e4355382d699482b07404d26
Eliot <ringevaleliot@hotmail.fr>
2020-04-26 12:00:28 +0200

    Compter les cuirs de pêche ou observer la collection de timbre

---
+++ Dev/BlackWater Crimes Project/Assets/Scripts/EliotDev/AttorneySampleScene/InterrogatingScene/QAndAScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class QAndAScript: MonoBehaviour
+{
+    [System.Serializable]
+    public struct QuestionAndAnswer
+    {
+        [Title("Question", bold: false)]
+        [HideLabel]
+        [MultiLineProperty(2)]
+        public string question;
+
+        [Title("Answer", bold: false)]
+        [HideLabel]
+        [MultiLineProperty(4)]
+        public string answer;
+    }
+
+    public QuestionAndAnswer[] whiteQAndA;
+    public QuestionAndAnswer[] andersonQAndA;
+    public QuestionAndAnswer[] jenkinsQAndA;
+    public QuestionAndAnswer[] morettiQAndA;
+}
