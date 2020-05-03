abde3e865e75cddc765a9a5b1cec5387fa60a4de
Majax le Chroniqueur <t.mergem@rubika-edu.com>
2020-05-03 22:47:29 +0200

    jhgfyvcdgswfcvhfxwdjkhfhygjughgjgfjkhjg

--- Dev/BlackWater Crimes Project/Assets/Scripts/EliotDev/NotesScene/NotesRecorder.cs
+++ Dev/BlackWater Crimes Project/Assets/Scripts/EliotDev/NotesScene/NotesRecorder.cs
@@ -70,6 +70,7 @@ public class NotesRecorder : MonoBehaviour
     void CloseEditNote()
     {
         currentNote.text = keyboard.text;
+        keyboard = null;
         
         if (!newNote)
         {
