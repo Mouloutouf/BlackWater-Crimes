90a9288d0b83ccb0827faad584b1f43be98606df
Eliot <ringevaleliot@hotmail.fr>
2020-06-07 13:40:46 +0200

    Un James Walker beau comme un sou neuf

--- Dev/BlackWater Crimes Project/Assets/Scripts/EliotDev/HeadHunter Scripts/DetailScript.cs
+++ Dev/BlackWater Crimes Project/Assets/Scripts/EliotDev/HeadHunter Scripts/DetailScript.cs
@@ -10,6 +10,17 @@ public class DetailScript : MonoBehaviour
     [SerializeField] List<string> facialHairOptions;
     [SerializeField] List<string> hairColorOptions;
     [SerializeField] List<string> particularPhysicalTraitOptions;
+    public List<string> categories = new List<string>();
+    bool firstClick = true;
+
+    private void Start() 
+    {
+        GetComponent<Dropdown>().enabled = false;
+        GetComponent<Dropdown>().captionText.GetComponent<Text>().fontStyle = FontStyle.Italic;
+        Color tempColor = GetComponent<Dropdown>().captionText.GetComponent<Text>().color;
+        tempColor.a = .7f;
+        GetComponent<Dropdown>().captionText.GetComponent<Text>().color = tempColor;
+    }
 
     public void UpdateDetailDropdown()
     {
@@ -35,6 +46,28 @@ public class DetailScript : MonoBehaviour
                 detailDropdown.ClearOptions();
                 detailDropdown.AddOptions(particularPhysicalTraitOptions);
                 return;
+        }
+    }
+
+    public void ErasePlaceHolderText()
+    {
+        if(firstClick)
+        {
+            firstClick = false;
+
+            GetComponent<Dropdown>().enabled = true;
+            GetComponent<Dropdown>().captionText.GetComponent<Text>().fontStyle = FontStyle.Normal;
+            Color tempColor = GetComponent<Dropdown>().captionText.GetComponent<Text>().color;
+            tempColor.a = 1f;
+            GetComponent<Dropdown>().captionText.GetComponent<Text>().color = tempColor;
+
+            GetComponent<Dropdown>().ClearOptions();
+            GetComponent<Dropdown>().AddOptions(categories);
+            GetComponent<Dropdown>().RefreshShownValue();
+            GetComponent<Dropdown>().Show();
+
+            detailDropdown.gameObject.SetActive(true);
+            detailDropdown.RefreshShownValue();
         }
     }
 }
